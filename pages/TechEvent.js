c8649623b434370ed1dcd4f9402fd2b2b59e156e
AviKKi <atul7555@gmail.com>
2020-02-11 00:44:46 +0530

    events page partial progress

--- pages/TechEvent.js
+++ pages/TechEvent.js
@@ -14,7 +14,7 @@ import GridItem from "../components/Grid/GridItem";
 import NavPills from "../components/NavPills/NavPills";
 import pillsStyle from "../components/NavPills/pillsStyle.jsx";
 import SubEvent from "../components/Event/SubEvent";
-
+import Newspaper from '../components/Newspaper'
 import Layout from "../components/Layout/Layout"
 //import Events from "../data/Events"
 import { Technical } from "../data/Events"
@@ -23,7 +23,7 @@ class SectionPills extends React.Component {
   render() {
     const headEvents = Technical
     const { classes } = this.props
-        const tabs = headEvents.map( val=>{
+    const tabs = headEvents.map(val => {
       return {
         tabButton: val.tabButton,
         tabIcon: val.tabIcon,
@@ -33,19 +33,52 @@ class SectionPills extends React.Component {
           </span>
         )
       }
-    } )
+    })
     return (
       <Layout>
-      <div className={classes.section} style={{width:'100%'}}>
-        <div className={classes.container}>
-                <NavPills
-                  color="success"
-                  tabs={tabs}
-                />
+        <div className={classes.section} style={{ width: '100%', marginVerticle: "30px" }}>
+          <div className={classes.container} >
+            <Newspaper.Wrapper style={styles.wrapper}>
+              {/* <Newspaper.TopHr /> */}
+              <Newspaper.H1 style={styles.heading}><img height="150px" src="../static/img/ribbon_robotics.png" /></Newspaper.H1>
+              <div style={styles.mainContent}>
+                <Newspaper.H2><u>Maze Runner</u></Newspaper.H2><br />
+                <Newspaper.Cursive>
+                  Maze runner is an event where you need to use all of your mechanical skills,
+                  coding skills and problem solving skills altogether to design a bot which can
+                  overcome the obstacles on its way and find a way to the destination by its own
+                  using the smartness coded into it by its creaters.
+                </Newspaper.Cursive>
+              </div>
+            </Newspaper.Wrapper>
+            <NavPills color="success" tabs={tabs} />
+          </div>
         </div>
-      </div>
       </Layout>
     );
+  }
+}
+
+const styles = {
+  wrapper: {
+    overflow: 'hidden',
+    position: 'relative',
+    width: "100%",
+  },
+  heading: {
+    textAlign: "center",
+    flex: 5
+  },
+  mainContent: {
+    border: '1px solid black',
+    height: "80%",
+    width: "98%",
+    position: 'absolute',
+    top: '180px',
+    padding: '10px 30px',
+    left: '0',
+    marginRight: '10px',
+    marginLeft: '10px',
   }
 }
 
